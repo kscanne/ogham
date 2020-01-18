15db60d1f533ac539e831aa2922624da71252310
Kevin Scannell <kscanne@gmail.com>
2020-01-18 09:44:16 -0600

    simpler as sed script

--- opp2pl.pl
+++ opp2pl.pl
@@ -1,46 +1,20 @@
-#!/usr/bin/perl
-
-use strict;
-use warnings;
-use utf8;
-
-binmode STDIN, ":utf8";
-binmode STDOUT, ":utf8";
-binmode STDERR, ":utf8";
-
-my %map = (
-	'ᚁ' => '$r[0]--',
-	'ᚂ' => '$r[1]--',
-	'ᚃ' => '$r[2]--',
-	'ᚄ' => '$r[3]--',
-	'ᚅ' => '$r[4]--',
-	'ᚆ' => '$r[0]++',
-	'ᚇ' => '$r[1]++',
-	'ᚈ' => '$r[2]++',
-	'ᚉ' => '$r[3]++',
-	'ᚊ' => '$r[4]++',
-	'᚛' => 'do{',
-	'᚜' => ';}',
-	'ᚋ' => 'while($r[0])',
-	'ᚌ' => 'while($r[1])',
-	'ᚍ' => 'while($r[2])',
-	'ᚎ' => 'while($r[3])',
-	'ᚏ' => 'while($r[4])',
-	' ' => ';',
-	'ᚕ' => ';print "@r\n";exit(0);',
-);
-
-sub opp2perl {
-	(my $s) = @_;
-	$s =~ s/(.)/$map{$1}/eg;
-	return $s;
-}
-
-my $p;
-while (<STDIN>) {
-	chomp;
-	$p .= $_;
-}
-print '@r = (0,0,0,0,0);'.opp2perl($p);
-
-exit 0;
+s/ᚁ/$r[0]--/g
+s/ᚂ/$r[1]--/g
+s/ᚃ/$r[2]--/g
+s/ᚄ/$r[3]--/g
+s/ᚅ/$r[4]--/g
+s/ᚆ/$r[0]++/g
+s/ᚇ/$r[1]++/g
+s/ᚈ/$r[2]++/g
+s/ᚉ/$r[3]++/g
+s/ᚊ/$r[4]++/g
+s/᚛/do{/g
+s/᚜/;}/g
+s/ᚋ/while($r[0])/g
+s/ᚌ/while($r[1])/g
+s/ᚍ/while($r[2])/g
+s/ᚎ/while($r[3])/g
+s/ᚏ/while($r[4])/g
+s/ /;/g
+s/ᚕ/;print "@r\\n";exit(0);/g
+s/^/@r=(0,0,0,0,0);/
