1953e350b16ede12a56ef24241ebb738de85d00f
Kevin Scannell <kscanne@gmail.com>
2020-01-18 15:00:41 -0600

    redo python as while vs. do/while also

--- opp2py.pl
+++ opp2py.pl
@@ -19,33 +19,50 @@ my %map = (
 	'ᚈ' => 'r[2]+=1',
 	'ᚉ' => 'r[3]+=1',
 	'ᚊ' => 'r[4]+=1',
-	'᚛' => "while True:\n",
-	'᚜' => "\n",
-	'ᚋ' => "if r[0]==0:\n",
-	'ᚌ' => "if r[1]==0:\n",
-	'ᚍ' => "if r[2]==0:\n",
-	'ᚎ' => "if r[3]==0:\n",
-	'ᚏ' => "if r[4]==0:\n",
-	' ' => "\n",
 	'ᚕ' => "\nprint(' '.join([str(x) for x in r]))\nexit(0)",
 );
 
-sub opp2perl {
-	(my $s) = @_;
-	my $ans='';
-	my $indent='';
-	while ($s =~ m/(.)/g) {
-		my $c = $1;
-		$ans .= $indent.$map{$c};
-		if ($c =~ m/^[ᚋᚌᚍᚎᚏ]$/) {
-			$ans .= "${indent}  break\n";
-			$indent =~ s/^  //;
+sub block2py {
+	(my $s, my $indent) = @_;
+	(my $inside, my $reg) = $s =~ m/^᚛(.*)᚜([ᚋᚌᚍᚎᚏ])$/;
+	my $num = ord($reg) - ord('ᚋ');
+	return "${indent}while r[$num]!=0:\n".opp2py($inside,$indent.'  ');
+}
+
+sub opp2py {
+	(my $s, my $indent) = @_;
+	if ($s =~ m/^([^᚛]*)(᚛.*)$/) {
+		my $pre = $1;
+		my $rest = $2;
+		my $block='';
+		my $depth = 0;
+		my $tail='';
+		my $in_tail_p = 0;
+		while ($rest =~ m/(.)/g) {
+			my $c = $1;
+			if ($in_tail_p) {
+				$tail .= $c;
+			}
+			else {
+				$block .= $c;
+			}
+			if ($c =~ m/^[ᚋᚌᚍᚎᚏ]$/) {
+				$in_tail_p = 1 if ($depth==0);
+			}
+			elsif ($c eq '᚛') {
+				$depth++;
+			}
+			elsif ($c eq '᚜') {
+				$depth--;
+			}
 		}
-		elsif ($c eq '᚛') {
-			$indent .= '  ';
-		}
+		#print "recursing with pre=$pre, block=$block, tail=$tail\n";
+		return opp2py($pre,$indent).block2py($block,$indent).opp2py($tail,$indent);
 	}
-	return $ans;
+	else {
+		$s =~ s/(.)/$indent.$map{$1}."\n"/eg;
+		return $s;
+	}
 }
 
 my $p='';
@@ -53,6 +70,6 @@ while (<STDIN>) {
 	chomp;
 	$p .= $_;
 }
-print "r = [0,0,0,0,0]\n".opp2perl($p);
+print "r = [0,0,0,0,0]\n".opp2py($p,'');
 
 exit 0;
